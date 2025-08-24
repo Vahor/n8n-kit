ceec2f61845cd9e5fa8e768806d2d3de607e406f
Vahor <me@vahor.fr>
2025-08-24 09:21:13 +0200

    remove the need to load submodules for typecheck job

--- packages/n8n-kit/src/nodes/schedule-trigger.ts
+++ packages/n8n-kit/src/nodes/schedule-trigger.ts
@@ -1,6 +1,49 @@
-import type { Rule } from "n8n-nodes-base/nodes/Schedule/SchedulerInterface";
+import type { CronExpression } from "n8n-workflow";
 import { type, version } from "../generated/nodes/ScheduleTrigger";
 import { Node, type NodeProps } from "./node";
+
+export type ScheduleInterval =
+	| {
+			field: "cronExpression";
+			expression: CronExpression;
+	  }
+	| {
+			field: "seconds";
+			secondsInterval: number;
+	  }
+	| {
+			field: "minutes";
+			minutesInterval: number;
+	  }
+	| {
+			field: "hours";
+			hoursInterval: number;
+			triggerAtMinute?: number;
+	  }
+	| {
+			field: "days";
+			daysInterval: number;
+			triggerAtHour?: number;
+			triggerAtMinute?: number;
+	  }
+	| {
+			field: "weeks";
+			weeksInterval: number;
+			triggerAtDay: number[];
+			triggerAtHour?: number;
+			triggerAtMinute?: number;
+	  }
+	| {
+			field: "months";
+			monthsInterval: number;
+			triggerAtDayOfMonth?: number;
+			triggerAtHour?: number;
+			triggerAtMinute?: number;
+	  };
+
+export interface Rule {
+	interval: ScheduleInterval[];
+}
 
 export interface ScheduleTriggerProps extends NodeProps {
 	parameters: {
