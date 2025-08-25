4b1b09473f50e15d7752a8812e2bea6fe73f2d6a
Vahor <me@vahor.fr>
2025-08-25 13:05:31 +0200

    add failing test

--- packages/n8n-kit/tests/nodes/if.test.ts
+++ packages/n8n-kit/tests/nodes/if.test.ts
@@ -1,4 +1,5 @@
 import { describe, test } from "bun:test";
+import { Chain } from "../../src";
 import { If } from "../../src/nodes";
 import { NoOp } from "../../src/nodes/generated";
 
@@ -35,6 +36,22 @@ describe("If", () => {
 				.false(new NoOp("false"))
 				// this works
 				.true(new NoOp("false"));
+		});
+	});
+
+	describe("type test, chaining if -> true/false -> connect, we have access to the context of the if", () => {
+		test("can connect to if true/false nodes", () => {
+			const _ = Chain.start(new NoOp("start"))
+				.next(
+					new If("if", {
+						conditions: [],
+					})
+						.true(new NoOp("true"))
+						.false(new NoOp("false")),
+				)
+				// TODO: https://github.com/Vahor/n8n-kit/issues/14
+				// @ts-expect-error: we have access to the context of the if
+				.connect(["true", "false"], new NoOp("connect"));
 		});
 	});
 });
