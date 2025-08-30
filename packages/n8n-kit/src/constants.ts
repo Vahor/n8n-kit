1a685d220dc005feda6aca6ff3e9543f1d5f6a8a
Vahor <me@vahor.fr>
2025-08-30 18:12:41 +0200

    add default salt when used in test

--- packages/n8n-kit/src/constants.ts
+++ packages/n8n-kit/src/constants.ts
@@ -12,6 +12,10 @@ export const setProjectSalt = (salt: string) => {
 
 export const getProjectSalt = (): string => {
 	if (projectSalt === null) {
+		if (process.env.NODE_ENV === "test") {
+			// add a default salt during tests
+			return "test";
+		}
 		throw new Error(
 			"Project salt not set. This should be set by the CLI before running workflows.",
 		);
