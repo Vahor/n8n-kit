70ecadb6739b22df578c965fafc8beec4c9d72bb
Vahor <me@vahor.fr>
2025-09-17 22:32:54 +0200

    happy ts

--- packages/n8n-kit/src/nodes/nasa.ts
+++ packages/n8n-kit/src/nodes/nasa.ts
@@ -1,18 +1,18 @@
 import { Nasa as _Nasa, type NasaProps } from "../generated/nodes-impl/Nasa";
 
+// @ts-expect-error: we override the parameters type
 export class Nasa<L extends string, P extends NasaProps> extends _Nasa<
 	L,
 	"parameters" extends keyof P
 		? NonNullable<P["parameters"]>["resource"] extends "donkiSolarFlare"
 			? { classType: string }
 			: { __TODO: string }
-		: never,
-	P
+		: never
 > {
 	constructor(
 		id: L,
 		override props: P,
 	) {
-		super(id, props as any);
+		super(id, props as never);
 	}
 }
