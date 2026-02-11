e37fcb438a9281457a1d5bebca428f19958118e3
Nathan <17593209+Vahor@users.noreply.github.com>
2026-02-11 20:47:08 +0100

    feat: add @n8n/ai-utilities in dev deps (#192)
    * feat: add n8n/ai-utils in deps
    
    * fix: language "python" no longer exist
    
    * fix: add back ! for types
    
    * update error message

--- packages/n8n-kit/src/nodes/webhook.ts
+++ packages/n8n-kit/src/nodes/webhook.ts
@@ -24,14 +24,15 @@ export interface WebhookProps
 type GetOutputSchemaField<
 	P extends WebhookProps,
 	F extends keyof NonNullable<WebhookProps["outputSchema"]>,
-> = IsNever<P["outputSchema"]> extends true
-	? never
-	: F extends keyof P["outputSchema"]
-		? IsNever<P["outputSchema"][F]> extends true
-			? never
-			: // @ts-ignore: ts doesn't know what IsNever is
-				P["outputSchema"][F]["infer"]
-		: never;
+> =
+	IsNever<P["outputSchema"]> extends true
+		? never
+		: F extends keyof P["outputSchema"]
+			? IsNever<P["outputSchema"][F]> extends true
+				? never
+				: // @ts-ignore: ts doesn't know what IsNever is
+					P["outputSchema"][F]["infer"]
+			: never;
 
 // @ts-expect-error: we override the type. TODO: I think we can fix this
 export class Webhook<L extends string, P extends WebhookProps> extends _Webhook<
